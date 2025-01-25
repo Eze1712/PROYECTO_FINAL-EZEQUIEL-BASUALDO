a7965e3f833ba79b05fb8a3572403552d940043b
Ezequiel Basualdo <ezequiel_francisco@hotmail.es>
2025-01-25 19:05:05 -0300

    commit

--- Main/forms.py
+++ Main/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Ingrese un nombre de usuario único'}))
     first_name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Ingrese su nombre'}))
     last_name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Ingrese su apellido'}))
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Ingrese su correo electrónico'}))
@@ -11,5 +12,4 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
-
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
