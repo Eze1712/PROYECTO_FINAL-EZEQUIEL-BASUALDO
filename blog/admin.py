90835bde2c7b6cb0a7846f807a1674ea62991856
Ezequiel Basualdo <ezequiel_francisco@hotmail.es>
2025-01-26 03:14:52 -0300

    1/26

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Leyenda
+from .models import *
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -14,3 +14,18 @@ class LeyendaAdmin(admin.ModelAdmin):
     list_filter = ["genero"]
     search_fields = ["nombre", "genero"]
     ordering = ["nombre"]
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'fecha_lanzamiento', 'genero', 'autor_album')
+    search_fields = ('nombre', 'genero', 'autor_album')
+    list_filter = ('genero', 'autor_album')
+    ordering = ('-fecha_lanzamiento',)
+
+    def get_queryset(self, request):
+        """Filtrar los álbumes solo a los que el usuario ha creado"""
+        queryset = super().get_queryset(request)
+        if not request.user.is_superuser:
+            queryset = queryset.filter(autor_album=request.user)
+        return queryset
+
